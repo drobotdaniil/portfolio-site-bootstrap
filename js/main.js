0453b549f31003a86bd0a637a8bd2b9be5b65565
Daniil Drobot <funplacebo7@gmail.com>
2019-07-22 19:40:44 +0300

    smoothly scroll to anchor

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ var lazyLoadInstance = new LazyLoad({
     elements_selector: ".lazy"
     // ... more custom settings?
 });
+
+//scroll to anchor
+$(".menu__item a").click(function(){
+    var element = $(this).attr("href");
+    var dist = $(element).offset().top;
+    $("html, body").animate({"scrollTop": dist}, 1000);
+
+    return false;
+});
